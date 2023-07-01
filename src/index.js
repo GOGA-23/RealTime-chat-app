3a180d61a2c70fb526edafdf185d0e8d1bf12815
Gokul Gandhi <gokulgandhi97@gmail.com>
2023-07-01 11:33:15 +0530

    firebase hosted

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./sass/style.scss";
 import App from "./App";
+// import "dotenv/config";
 import { BrowserRouter } from "react-router-dom";
 import { UserAuthContextProvider } from "./context/UserAuthContext";
 import { ChatContextProvider } from "./context/ChatAuth";
